2f93b85dce52e07fc2f0d45d6f3e7179ace81bc5
nupur-bhatt <nupurbhatt999@gmail.com>
2026-02-18 05:35:28 -0400

    edited server.js to remove cors error

--- server.js
+++ server.js
@@ -5,11 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(
-  cors({
-    origin: "http://localhost:5173", 
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
